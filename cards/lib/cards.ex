fc4788c6dfa7c44846c155f2cdddd4a0c52516a1
mwdowns <mwdowns@gmail.com>
2018-05-12 15:51:38 -0400

    better create_deck method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -17,10 +17,40 @@ defmodule Cards do
   end
 
   def create_deck do
-    ["Ace", "Two", "Three", "Four"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # WRONG WAY - because it returns nested arrays
+    # for suit <- suits do
+    #   for value <- values do
+    #     value <> " of " <> suit
+    #     # "#{value} of #{suit}"
+    #   end
+    # end
+
+    # RIGHT WAY
+    # OPTION 1 - better, returns a flattened array
+    # cards = for suit <- suits do
+    #   for value <- values do
+    #     value <> " of " <> suit
+    #     # "#{value} of #{suit}"
+    #   end
+    # end
+
+    # List.flatten(cards)
+
+    # OPTION 2 - best because it's shorter
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
