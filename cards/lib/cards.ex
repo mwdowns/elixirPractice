481cd4fdc7b77e57d708709cfeaea7fc51076b24
mwdowns <mwdowns@gmail.com>
2018-05-13 12:54:54 -0400

    Adds better documentation

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,6 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+    Methods for creating and handling a deck of cards
   """
 
   @doc """
@@ -16,6 +16,18 @@ defmodule Cards do
     :world
   end
 
+  @doc """
+  Creates a deck of cards.
+
+  Returns:
+  * a `deck` (list of strings) representing a deck of 52 cards - 4 suits of 13 values
+
+  ## Examples
+
+      iex> Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", ...]
+
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -46,23 +58,104 @@ defmodule Cards do
 
   end
 
+  @doc """
+  Shuffles cards within a deck.
+
+  Argument:
+  * `deck` (list of strings)
+
+  Returns:
+  * a new `deck` with the strings stuffled
+
+  ## Examples
+
+      iex> Cards.shuffle(deck)
+      ["Two of Hearts", "King of Spades", "Ten of Spades", ...]
+
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Checks to see if a card is within a deck.
+
+  Arguments:
+  * `deck` (list of strings)
+  * `card` (single string)
+
+  Returns:
+  * `true` or `false`.
+
+  ## Examples
+
+      iex> Cards.contains?(deck, "Two of Hearts")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Creates hand and a remaining deck from an initial deck given a hand_size.
+
+  Arguments:
+  * `deck` (list of strings)
+  * `hand_size` (int)
+
+  Returns:
+  * Tupul {`hand` (list of strings of `hand_size`), `rest_of_deck` (list of strings minus `hand_size`)}
+
+  ## Examples
+
+      iex> Cards.deal(deck, 5)
+      {["card", "card"], ["card", "card", "card"]}
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+  Saves a deck to the hard-drive.
+
+  Arguments:
+  * `deck` (list of strings)
+  * `filename` (string)
+
+  Returns:
+  * :ok
+
+  ## Examples
+
+      iex> Cards.save(deck, "my_file")
+      :ok
+
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+  Loads a deck from the hard-drive.
+
+  Arguments:
+  * `filename` (string)
+
+  Returns:
+  * `deck` (list of strings) if successful or `error message` (string) if unsuccessful.
+
+  ## Examples
+
+      iex> Cards.load("my_file")
+      ["card", "card", "card", ...]
+
+      ie> Cards.load("garbage")
+      "That file does not exist"
+
+  """
   def load(filename) do
 
     case File.read(filename) do
@@ -72,6 +165,21 @@ defmodule Cards do
 
   end
 
+  @doc """
+  Runs 3 functions using the pipe opporator: `create_deck` -> `shuffle` -> `deal(hand_size)`
+
+  Arguments:
+  * `hand_size` (int)
+
+  Returns:
+  * Tupul {`hand` (list of strings of `hand_size`), `rest_of_deck` (list of strings minus `hand_size`)}
+
+  ## Examples
+
+      iex> Cards.create_hand(5)
+      {["card", "card"], ["card", "card", "card"]}
+
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
